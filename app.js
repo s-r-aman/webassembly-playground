c34b203348d2e96c7e1faaed6ad831cf50f07e95
s-r-aman <s.r.aman.sra@gmail.com>
2018-05-14 18:11:07 +0530

    Implementaiton of other functions.

--- app.js
+++ app.js
@@ -17,10 +17,16 @@ function loadWasm(file) {
 //     .then(bit => WebAssembly.compile(bit))
 //     .then(module => WebAssembly.instance(module));
 
-var multiply;
+var multiply, sum, divide, subract;
 
 loadWasm('main.wasm').then(function(instance) {
   multiply = instance.exports._Z8multiplyii;
+  sum = instance.exports._Z3sumii;
+  divide = instance.exports._Z6divideii;
+  subract = instance.exports._Z8subtractii;
   console.log('Finished compiling Wasm file!');
-  console.log(multiply(4, 5));
+  console.log('Result of multiplying 4 and 5 :', multiply(4, 5));
+  console.log('Result of suming 4 and 5 :', sum(4, 5));
+  console.log('Result of dividing 4 and 5 :', divide(4, 5));
+  console.log('Result of subracting 4 and 5 :', subract(4, 5));
 });
