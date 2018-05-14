baf7aa3b91e667b58a35817175baccc5266628e0
s-r-aman <s.r.aman.sra@gmail.com>
2018-05-14 18:06:11 +0530

    Added some more funtions.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
-int multiply(int a, int b) {
+int multiply(int a, int b)
+{
   return a * b;
+}
+
+int sum(int a, int b)
+{
+  return a + b;
+}
+
+int subtract(int a, int b)
+{
+  return a - b;
+}
+
+float divide(int a, int b)
+{
+  return a / b;
 }
